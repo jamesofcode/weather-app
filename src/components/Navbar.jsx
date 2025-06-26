f7262e4228fe72df7644c3883f0944853c4dd8fa
jamesofcode <jameskstanton123@gmail.com>
2025-06-26 21:25:05 +0200

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import { SiTheweatherchannel } from "react-icons/si";
-import { NavLink } from 'react-router';
+import { NavLink } from 'react-router-dom'; // fixed import
+
 const Navbar = () => {
   return (
-    <div className='bg-green-700'>
-        <SiTheweatherchannel className='inline text-lg mb-1 ml-2' />
-        <div className='inline'>
-            <NavLink
-                to='/'
-            >
-                Home 
-            </NavLink>
-            <NavLink>
-                All Cities
-            </NavLink>
-        </div>
-    </div>
-  )
+    <nav className="flex items-center justify-between bg-blue-500 text-white p-4 shadow-md">
+      <div className="flex items-center space-x-2">
+        <SiTheweatherchannel className="text-2xl" />
+        <span className="font-bold text-lg">WeatherApp</span>
+      </div>
+      <div className="flex space-x-6">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => 
+            `hover:underline ${isActive ? 'font-semibold bg-blue-900 rounded px-2' : ''}`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink 
+          to="/cities" 
+          className={({ isActive }) => 
+            `hover:underline ${isActive ? 'font-semibold bg-blue-900 rounded px-2' : ''}`
+          }
+        >
+          All Cities
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
